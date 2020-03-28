a3115bc1d47e52393ce1da223f10c7a2ea6fc784
Сергей <sag163@mail.ru>
2020-03-28 15:43:23 +0400

    edit group.html and post/view/group

--- posts/views.py
+++ posts/views.py
@@ -18,8 +18,11 @@ def index(request):
 
 def group_post(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]  
-    return render(request, "group.html", {"group": group, "posts": posts})
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page') 
+    page = paginator.get_page(page_number) 
+    return render(request, "group.html", {"group": group, "page": page, 'paginator':paginator})
 
 @login_required
 def new_post(request):
